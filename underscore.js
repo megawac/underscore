e942b43986b102f1291ac9c368f23a56c1eb8cd3
megawac <yeatesgraeme@gmail.com>
2014-07-14 01:33:50 -0400

    lookupIterator: Simpler object check

--- underscore.js
+++ underscore.js
@@ -81,7 +81,7 @@
   var lookupIterator = function(value, context, argCount) {
     if (value == null) return _.identity;
     if (_.isFunction(value)) return createCallback(value, context, argCount);
-    if (_.isObject(value)) return _.matches(value);
+    if (typeof value == 'object') return _.matches(value);
     return _.property(value);
   };
 
